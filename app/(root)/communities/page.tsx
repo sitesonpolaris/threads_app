dc7a1ad524e35050d40540129983b7e0734a6a7d
Your Name <you@example.com>
2023-09-28 15:38:07 -0400

    first commit

---
+++ app/(root)/communities/page.tsx
@@ -0,0 +1,11 @@
+const Page = async () => {
+    return (
+        <section>
+            <h1 className="head-text mb-19">
+                Communities
+            </h1>
+        </section>
+    )
+}
+
+export default Page;
