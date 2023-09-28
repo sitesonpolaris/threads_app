2fcc26d69dcb2d143bb10906c3eeceaa68b4ead8
Your Name <you@example.com>
2023-09-28 15:58:21 -0400

    ignore ts errors 2

--- next.config.js
+++ next.config.js
@@ -2,10 +2,9 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-  }
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   experimental: {
     serverActions: true,
     serverComponentsExternalPackages: ["mongoose"],
